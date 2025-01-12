a34af78e51c6be781eb7c004adb9d00016b91e1d
pooyafils <pooyafils@gmail.com>
2025-01-12 12:50:15 +0330

    allow orgin

--- src/main/java/com/example/websocket/config/WebSocketConfig.java
+++ src/main/java/com/example/websocket/config/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:63342")
+                .setAllowedOriginPatterns("*") // Allow all origins using a pattern
                 .withSockJS();
     }
 }
